e37142632f008ff0289453c3a7923b99d1cd904c
me <me@local.host>
2026-07-06 20:19:04 +0700

    adjusting

--- fs/exfat/fatent.c
+++ fs/exfat/fatent.c
@@ -262,10 +262,7 @@ int exfat_zeroed_cluster(struct inode *dir, unsigned int clu)
 	}
 
 	if (IS_DIRSYNC(dir))
-		return sync_blockdev_range(sb->s_bdev,
-				EXFAT_BLK_TO_B(blknr, sb),
-				EXFAT_BLK_TO_B(last_blknr, sb) - 1);
-
+		return sync_blockdev(sb->s_bdev,);
 	return 0;
 }
 
